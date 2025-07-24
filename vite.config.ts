b9afb96a8fdcbd728e07eb8ea73482eff07003eb
addy-hypr4 <adhbhut.gupta@hypr4.io>
2025-07-24 12:09:37 +0530

    deployed on github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from "path"
 
 // https://vitejs.dev/config/
-export default defineConfig(() => ({
-  server: {
-    host: "::",
-    port: 3700,
-  },
-  plugins: [
-    react(),
-  ],
+export default defineConfig({
+  plugins: [react()],
+  // Set the base path for deployment
+  base: '/adhbhut/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+})
