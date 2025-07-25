2379d02407cdc898b11ff2eaa8078ad685a7601c
addy-hypr4 <adhbhut.gupta@hypr4.io>
2025-07-25 15:08:12 +0530

    updated react router

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,8 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      {/* Add basename="/adhbhut" */}
+      <BrowserRouter basename="/adhbhut">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
