1f878fcc076cdd15339b55d7af121d8bd47d9905
addy-hypr4 <adhbhut.gupta@hypr4.io>
2025-10-29 16:19:11 +0530

    fixed deployment path issue

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      {/* Add basename="/adhbhut" */}
-      <BrowserRouter>
+      <BrowserRouter basename="/adhbhut">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
